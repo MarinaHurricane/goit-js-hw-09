4a9adddbc24b2af768d88148866bd91da097aeed
MarinaHurricane <maryishch88@gmail.com>
2025-10-20 01:36:51 +0100

    change of form js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,55 @@
+// let formData = {
+//     email: "",
+//     message: ""
+// }
+
+
+// const form = document.querySelector(".feedback-form");
+// const email = document.querySelector('input[name="email"]');
+// const textarea = document.querySelector('textarea[name="message"]');
+// const localStorageKey = "feedback-form-state";
+
+// form.addEventListener("input", inputFunction);
+
+// function inputFunction(event) {
+//     formData[event.target.name] = event.target.value.trim();
+//     localStorage.setItem(localStorageKey, JSON.stringify(formData));
+// }
+
+// const savedFormData = localStorage.getItem(localStorageKey);
+// if(savedFormData) {
+//     formData = JSON.parse(savedFormData);
+//     email.value = formData.email ?? "";
+//     textarea.value = formData.message ?? "";
+// }
+
+
+// form.addEventListener("submit", submitFunction);
+
+// function submitFunction(event) {
+//     event.preventDefault();
+//     if (formData.email && formData.message) {
+//         console.log(formData);
+//         localStorage.removeItem(localStorageKey);
+//         form.reset();
+//     } else {
+//         alert("Please fill in both fields!")
+//     }
+// };
+
+
+
+
 let formData = {
     email: "",
     message: ""
 }
-
 
 const form = document.querySelector(".feedback-form");
 const email = document.querySelector('input[name="email"]');
 const textarea = document.querySelector('textarea[name="message"]');
 const localStorageKey = "feedback-form-state";
 
-form.addEventListener("input", inputFunction);
-
-function inputFunction(event) {
-    formData[event.target.name] = event.target.value.trim();
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-}
 
 const savedFormData = localStorage.getItem(localStorageKey);
 if(savedFormData) {
@@ -23,6 +58,10 @@ if(savedFormData) {
     textarea.value = formData.message ?? "";
 }
 
+form.addEventListener("input", (event) => { 
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+    formData[event.target.name] = event.target.value.trim();  
+});
 
 form.addEventListener("submit", submitFunction);
 
@@ -30,11 +69,13 @@ function submitFunction(event) {
     event.preventDefault();
     if (formData.email && formData.message) {
         console.log(formData);
-        localStorage.removeItem(localStorageKey);
-        form.reset();
     } else {
         alert("Please fill in both fields!")
     }
-
-  
+        localStorage.removeItem(localStorageKey);
+        formData.email = "";
+        formData.message = "";
+        form.reset();
 };
+
+
